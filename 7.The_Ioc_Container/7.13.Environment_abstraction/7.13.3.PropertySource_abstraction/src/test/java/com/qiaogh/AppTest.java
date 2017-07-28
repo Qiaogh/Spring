ef365c812dade4f4159fb8bab991f5b478e63c88
Qiaogh <qq892525597qq@163.com>
2017-07-29 07:30:46 +0800

    添加 7.13.3 PropertySource abstraction 示例

--- 7.The_Ioc_Container/7.13.Environment_abstraction/7.13.3.PropertySource_abstraction/src/test/java/com/qiaogh/AppTest.java
+++ 7.The_Ioc_Container/7.13.Environment_abstraction/7.13.3.PropertySource_abstraction/src/test/java/com/qiaogh/AppTest.java
@@ -6,9 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import com.qiaogh.domain.Man;
-import com.qiaogh.domain.Women;
+import org.springframework.core.env.Environment;
 
 /**
  * Unit test for simple App.
@@ -24,12 +22,19 @@ public class AppTest {
 
     @Test
     public void test() {
-        Man husband = cxt.getBean( Man.class );
-        Women wife = cxt.getBean( Women.class );
-        Assert.assertNotNull( husband );
-        Assert.assertNotNull( wife );
-        Assert.assertEquals( wife, husband.getWife() );
-        Assert.assertEquals( husband, wife.getHusband() );
+        Environment env = cxt.getEnvironment();
+        Assert.assertTrue( env.getProperty( "java.version", "null" ).equals( System.getProperty( "java.version" ) ) ); // Java 运行时环境版本
+        Assert.assertTrue( env.getProperty( "java.vendor", "null" ).equals( System.getProperty( "java.vendor" ) ) ); // Java 运行时环境供应商
+        Assert.assertTrue( env.getProperty( "java.vendor.url", "null" ).equals( System.getProperty( "java.vendor.url" ) ) ); // Java 供应商的 URL
+        Assert.assertTrue( env.getProperty( "java.home", "null" ).equals( System.getProperty( "java.home" ) ) ); // Java 安装目录
+        Assert.assertTrue( env.getProperty( "java.vm.specification.version", "null" ).equals( System.getProperty( "java.vm.specification.version" ) ) ); // Java 虚拟机规范版本
+        Assert.assertTrue( env.getProperty( "java.class.version", "null" ).equals( System.getProperty( "java.class.version" ) ) ); // Java 类格式版本号
+        Assert.assertTrue( env.getProperty( "java.class.path", "null" ).equals( System.getProperty( "java.class.path" ) ) ); // Java 类路径
+        Assert.assertTrue( env.getProperty( "os.name", "null" ).equals( System.getProperty( "os.name" ) ) ); // 操作系统名称
+        Assert.assertTrue( env.getProperty( "os.arch", "null" ).equals( System.getProperty( "os.arch" ) ) ); // 操作系统架构
+        Assert.assertTrue( env.getProperty( "os.version", "null" ).equals( System.getProperty( "os.version" ) ) ); // 操作系统般般
+        Assert.assertTrue( env.getProperty( "user.home", "null" ).equals( System.getProperty( "user.home" ) ) ); // 用户的主目录
+        Assert.assertTrue( env.getProperty( "user.dir", "null" ).equals( System.getProperty( "user.dir" ) ) ); // 用户的当前工作目录
     }
 
     @After
