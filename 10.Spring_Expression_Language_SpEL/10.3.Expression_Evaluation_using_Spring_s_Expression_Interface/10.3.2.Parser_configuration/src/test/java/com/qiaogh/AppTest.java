f0aacb506584c7bad803967dbbac3f7d007faff0
Qiaogh <qq892525597qq@163.com>
2017-09-05 15:56:39 +0800

    调整 10.3 Expression Evaluation using Spring’s Expression Interface 示例

---
+++ 10.Spring_Expression_Language_SpEL/10.3.Expression_Evaluation_using_Spring_s_Expression_Interface/10.3.2.Parser_configuration/src/test/java/com/qiaogh/AppTest.java
@@ -0,0 +1,45 @@
+package com.qiaogh;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.SpelParserConfiguration;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+import com.qiaogh.domain.Person;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void testSetGenericTypePropertyWithContext() {
+        String name = "Qiaogh";
+        String tellNumber = "15022730827";
+        
+        Person person = new Person();
+        
+        EvaluationContext context = new StandardEvaluationContext( person );
+        
+        SpelParserConfiguration parserConfiguration = new SpelParserConfiguration( true, true );
+        ExpressionParser expressionParser = new SpelExpressionParser( parserConfiguration );
+        
+        // expression of name
+        Expression nameExpression = expressionParser.parseExpression( "name" );
+        nameExpression.setValue( context, name );
+        
+        // expression of tells
+        Expression tellsExpression = expressionParser.parseExpression( "tellNumbers[0]" );
+        tellsExpression.setValue( context, tellNumber );
+        
+        Assert.assertNotNull( person.getName() );
+        Assert.assertTrue( !person.getTellNumbers().isEmpty() );
+        Assert.assertEquals( name, person.getName() );
+        Assert.assertEquals( 1, person.getTellNumbers().size() );
+        Assert.assertEquals( tellNumber, person.getTellNumbers().get( 0 ) );
+    }
+}
