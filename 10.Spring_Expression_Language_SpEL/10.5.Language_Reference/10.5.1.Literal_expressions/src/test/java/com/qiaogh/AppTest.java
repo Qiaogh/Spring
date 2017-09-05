fd237cc97c8683c19e41d048da9174c838042e7d
Qiaogh <qq892525597qq@163.com>
2017-09-05 16:27:36 +0800

    添加 10.5 Language Reference 示例

---
+++ 10.Spring_Expression_Language_SpEL/10.5.Language_Reference/10.5.1.Literal_expressions/src/test/java/com/qiaogh/AppTest.java
@@ -0,0 +1,54 @@
+package com.qiaogh;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void testLiteralString() {
+        ExpressionParser expressionParser = new SpelExpressionParser();
+        Expression expression = expressionParser.parseExpression( "'Qiaogh'" );
+        String value = expression.getValue( String.class );
+        Assert.assertEquals( "Qiaogh", value );
+    }
+    
+    @Test
+    public void testLiteralFloat() {
+        ExpressionParser expressionParser = new SpelExpressionParser();
+        Expression expression = expressionParser.parseExpression( "10.07" );
+        float value = expression.getValue( Float.class );
+        Assert.assertTrue( 10.07f == value );
+    }
+    
+    @Test
+    public void testLiteralInteger() {
+        ExpressionParser expressionParser = new SpelExpressionParser();
+        Expression expression = expressionParser.parseExpression( "10" );
+        int value = expression.getValue( Integer.class );
+        Assert.assertTrue( 10 == value );
+    }
+    
+    @Test
+    public void testLiteralBoolean() {
+        ExpressionParser expressionParser = new SpelExpressionParser();
+        Expression expression = expressionParser.parseExpression( "true" );
+        boolean value = expression.getValue( Boolean.class );
+        Assert.assertTrue( value );
+    }
+    
+    @Test
+    public void testLiteralNull() {
+        ExpressionParser expressionParser = new SpelExpressionParser();
+        Expression expression = expressionParser.parseExpression( "null" );
+        Object value = expression.getValue();
+        Assert.assertNull( value );
+    }
+}
