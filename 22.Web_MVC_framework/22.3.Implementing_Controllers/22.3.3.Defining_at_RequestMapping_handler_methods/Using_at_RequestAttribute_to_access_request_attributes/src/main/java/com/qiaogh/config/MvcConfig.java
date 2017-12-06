3e390731df1680521b02931558f234397f4d317d
Qiaogh <qq892525597qq@163.com>
2017-12-06 14:08:13 +0800

    调整示例代码

--- 22.Web_MVC_framework/22.3.Implementing_Controllers/22.3.3.Defining_at_RequestMapping_handler_methods/Using_at_RequestAttribute_to_access_request_attributes/src/main/java/com/qiaogh/config/MvcConfig.java
+++ 22.Web_MVC_framework/22.3.Implementing_Controllers/22.3.3.Defining_at_RequestMapping_handler_methods/Using_at_RequestAttribute_to_access_request_attributes/src/main/java/com/qiaogh/config/MvcConfig.java
@@ -4,11 +4,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan( basePackages = "com.qiaogh", includeFilters = {
     @ComponentScan.Filter( type = FilterType.ANNOTATION, value = Controller.class )
 })
 public class MvcConfig {
-    
+
 }
--- 22.Web_MVC_framework/22.3.Implementing_Controllers/22.3.3.Defining_at_RequestMapping_handler_methods/Using_at_SessionAttribute_to_access_pre_existing_global_session_attributes/src/main/java/com/qiaogh/controller/UserManageController.java
+++ 22.Web_MVC_framework/22.3.Implementing_Controllers/22.3.3.Defining_at_RequestMapping_handler_methods/Using_at_SessionAttribute_to_access_pre_existing_global_session_attributes/src/main/java/com/qiaogh/controller/UserManageController.java
@@ -1,16 +1,13 @@
 package com.qiaogh.controller;
 
+import com.qiaogh.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.qiaogh.domain.User;
 
 @Controller
 @RequestMapping( "/userManage" )
-@SessionAttributes( "user" )
 public class UserManageController {
     
     @RequestMapping( "/info" )
