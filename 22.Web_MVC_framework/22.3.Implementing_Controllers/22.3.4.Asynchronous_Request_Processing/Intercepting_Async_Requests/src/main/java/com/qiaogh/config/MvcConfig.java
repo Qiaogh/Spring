ca30c493d1e52c6125bc6c48c37d1da1cc5f5d9e
Qiaogh <qq892525597qq@163.com>
2017-12-13 17:45:42 +0800

    无

--- 22.Web_MVC_framework/22.3.Implementing_Controllers/22.3.4.Asynchronous_Request_Processing/Intercepting_Async_Requests/src/main/java/com/qiaogh/config/MvcConfig.java
+++ 22.Web_MVC_framework/22.3.Implementing_Controllers/22.3.4.Asynchronous_Request_Processing/Intercepting_Async_Requests/src/main/java/com/qiaogh/config/MvcConfig.java
@@ -3,14 +3,9 @@ package com.qiaogh.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebMvc
