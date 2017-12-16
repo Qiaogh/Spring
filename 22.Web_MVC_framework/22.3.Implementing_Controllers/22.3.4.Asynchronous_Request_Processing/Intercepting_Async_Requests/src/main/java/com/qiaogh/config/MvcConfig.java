60784ed346311dadb6252a1c18fe954e0e31c1bc
Qiaogh <qq892525597qq@163.com>
2017-12-16 15:35:36 +0800

    无

--- 22.Web_MVC_framework/22.3.Implementing_Controllers/22.3.4.Asynchronous_Request_Processing/Intercepting_Async_Requests/src/main/java/com/qiaogh/config/MvcConfig.java
+++ 22.Web_MVC_framework/22.3.Implementing_Controllers/22.3.4.Asynchronous_Request_Processing/Intercepting_Async_Requests/src/main/java/com/qiaogh/config/MvcConfig.java
@@ -1,11 +1,16 @@
 package com.qiaogh.config;
 
+import com.qiaogh.interceptor.AuthorityInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -16,5 +21,6 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addInterceptors( InterceptorRegistry registry ) {
+        registry.addInterceptor( new AuthorityInterceptor() );
     }
 }
---
+++ 22.Web_MVC_framework/22.3.Implementing_Controllers/22.3.4.Asynchronous_Request_Processing/Intercepting_Async_Requests/src/main/java/com/qiaogh/interceptor/AuthorityInterceptor.java
@@ -0,0 +1,38 @@
+package com.qiaogh.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.AsyncHandlerInterceptor;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class AuthorityInterceptor implements AsyncHandlerInterceptor {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger( AuthorityInterceptor.class );
+
+    @Override
+    public boolean preHandle( HttpServletRequest request, HttpServletResponse response, Object handler ) throws Exception {
+        LOGGER.info( "preHandle" );
+        return true;
+    }
+
+    @Override
+    public void postHandle( HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView ) throws Exception {
+        LOGGER.info( "postHandle" );
+    }
+
+    @Override
+    public void afterCompletion( HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex ) throws Exception {
+        LOGGER.info( "afterCompletion" );
+    }
+
+    @Override
+    public void afterConcurrentHandlingStarted( HttpServletRequest request, HttpServletResponse response, Object handler ) throws Exception {
+        LOGGER.info( "afterConcurrentHandlingStarted" );
+    }
+}
