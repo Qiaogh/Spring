e9594a95cd261984d6293f347204dcd42d43ff00
Qiaogh <qq892525597qq@163.com>
2017-12-19 09:46:39 +0800

    FIX-修复用户登录成功后，Service 没有打印用户信息的问题。

--- 22.Web_MVC_framework/22.4.Handler_mappings/src/main/java/com/qiaogh/config/AppConfig.java
+++ 22.Web_MVC_framework/22.4.Handler_mappings/src/main/java/com/qiaogh/config/AppConfig.java
@@ -2,11 +2,9 @@ package com.qiaogh.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 
 @Configuration
-@PropertySource( "classpath:log4j.properties" )
 @ComponentScan( basePackages = "com.qiaogh", excludeFilters = {
         @ComponentScan.Filter( Controller.class )
 })
