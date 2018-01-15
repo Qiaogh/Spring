93b46e98e65d9acef2e32f727ed9a2a3eaba50a5
Qiaogh <qq892525597qq@163.com>
2018-01-15 21:05:17 +0800

    添加 22.14 HTTP caching support 示例

---
+++ 22.Web_MVC_framework/22.13.Convention_over_configuration_support/22.13.3.Default_view_name/src/main/java/com/qiaogh/service/impl/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package com.qiaogh.service.impl;
+
+import com.qiaogh.domain.User;
+import com.qiaogh.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+import java.util.Date;
+
+@Service( "userService" )
+public class UserServiceImpl implements UserService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger( UserServiceImpl.class );
+
+    public User getByName( String name ) {
+        User user = new User();
+        user.setId( Integer.MAX_VALUE );
+        user.setName( name );
+        user.setPassword( "123" );
+        user.setAge( 26 );
+        user.setBirthday( buildBirthday() );
+
+        LOGGER.info( "用户登录成功！用户信息：{}", new Object[] { user } );
+        return user;
+    }
+
+    private static Date buildBirthday() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set( Calendar.YEAR, 1988 );
+        calendar.set( Calendar.MONTH, 9 );
+        calendar.set( Calendar.DAY_OF_MONTH, 7 );
+        return calendar.getTime();
+    }
+}
