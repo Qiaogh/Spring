1fc5821c0497ec5ec35914360b1a3304f22afe90
Qiaogh <qq892525597qq@163.com>
2018-02-19 16:32:35 +0800

    添加 23.2 Thymeleaf 示例。

---
+++ 23.View_Technologies/23.2.Thymeleaf/src/main/java/com/qiaogh/config/MvcConfig.java
@@ -0,0 +1,61 @@
+package com.qiaogh.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan( basePackages = "com.qiaogh", includeFilters = {
+        @ComponentScan.Filter({
+                Controller.class,
+                Service.class
+        })
+} )
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public ITemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix( "/WEB-INF/templates/" );
+        templateResolver.setSuffix( ".html" );
+        templateResolver.setTemplateMode( TemplateMode.HTML );
+        return templateResolver;
+    }
+
+    @Bean
+    public ITemplateEngine templateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver( templateResolver() );
+        return templateEngine;
+    }
+
+    /*
+     * Configure View Resolver
+     */
+    @Bean
+    @SuppressWarnings( "deprecation" )
+    public ViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine( templateEngine() );
+        return viewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers( ResourceHandlerRegistry registry ) {
+        registry.addResourceHandler( "/css/*" ).addResourceLocations( "/css/" );
+        registry.addResourceHandler( "/images/*" ).addResourceLocations( "/images/" );
+    }
+}
---
+++ 23.View_Technologies/23.2.Thymeleaf/src/main/java/com/qiaogh/controller/HomeController.java
@@ -0,0 +1,52 @@
+/*
+ * =============================================================================
+ *
+ *   Copyright (c) 2011-2016, The THYMELEAF team (http://www.thymeleaf.org)
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ * =============================================================================
+ */
+package com.qiaogh.controller;
+
+import java.util.Calendar;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.qiaogh.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+@Controller
+public class HomeController {
+
+    @ModelAttribute
+    public void addUserToSession( HttpSession session ) {
+        session.setAttribute( "user", new User( "John", "Apricot", "Antarctica", null ) );
+    }
+
+    @RequestMapping( "/" )
+    public String process( Model model ) {
+        model.addAttribute( "today", Calendar.getInstance() );
+        return "home";
+    }
+
+}
